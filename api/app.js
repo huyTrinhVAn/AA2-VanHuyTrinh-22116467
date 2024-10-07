50dbac8ac7618ce87910611b7d41e27beef056d8
huyTrinhVAn <trinhvanhuy11a9@gmail.com>
2024-10-07 12:31:31 +1100

    readme

--- api/app.js
+++ api/app.js
@@ -14,16 +14,7 @@ db.sequelize.sync({ force: true }).then(() => {
 
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
-app.use(express.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});
 
 require("./routes/contacts.routes")(app);
 require("./routes/phones.routes")(app);
@@ -35,4 +26,13 @@ require("./routes/stats.routes")(app);
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
+});// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
 });
